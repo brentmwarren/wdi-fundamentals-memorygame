3b9c7fa96f1e055a6cdb6c7e31a449efa44ec8bc
Brent Warren <macuser@Brentsmac.local>
2019-09-05 12:12:36 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,22 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardTwo);
+//console.log("User flipped " + cardThree);
+//console.log("User flipped " + cardFour);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+const cards = ["queen","queen", "king", "king"]
+const cardsInPlay = []
+
+var cardOne = cards[0]
+cardsInPlay.push(cardOne)
+console.log(cardOne)
+
+var cardTwo = cards[2]
+cardsInPlay.push(cardTwo)
+console.log(cardTwo)
+
+if ((cardsInPlay.length === 2) && (cardsInPlay[0] === cardsInPlay[1])) {
+alert("You found a match!");
+}
+else {
+alert("Sorry, try again.");
+}
