bc13ddc402328f6ea17014be9718cc3395ba32fe
Brent Warren <macuser@Brentsmac.local>
2019-09-06 15:25:01 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,7 +3,32 @@
 //console.log("User flipped " + cardThree);
 //console.log("User flipped " + cardFour);
 
-const cards = ["queen","queen", "king", "king"]
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
+},
+
+
+];
+
+
 const cardsInPlay = []
 
 function checkForMatch() {
@@ -17,8 +42,10 @@ else if ((cardsInPlay.length === 2) && (cardsInPlay[0] !== cardsInPlay[1])) {
 
 function flipCard(cardId) {
 
-console.log("User flipped " + cards[cardId])
-cardsInPlay.push(cards[cardId])
+console.log("User flipped " + cards[cardId].rank)
+console.log(cards[cardId].cardImage)
+console.log(cards[cardId].suit)
+cardsInPlay.push(cards[cardId].rank)
 checkForMatch()
  if ((cardsInPlay.length === 2) && (cardsInPlay[0] === cardsInPlay[1])) {
 alert("You found a match!");
