f67f4d768ed6e7c7fb3caed0a97537d5298b063b
Brent Warren <macuser@Brentsmac.local>
2019-09-07 19:30:40 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,31 +31,65 @@ cardImage: "images/king-of-diamonds.png",
 
 const cardsInPlay = []
 
+
 function checkForMatch() {
-if ((cardsInPlay[0] === cardsInPlay[1]) && (cardsInPlay.length === 2)) {
-  console.log("You found a match!");
-} 
-else if ((cardsInPlay.length === 2) && (cardsInPlay[0] !== cardsInPlay[1])) {
-  console.log("Sorry, try again.");
-}
+	if ((cardsInPlay[0] === cardsInPlay[1]) && (cardsInPlay.length === 2)) {
+  		alert("You found a match!");
+	} 
+	else if ((cardsInPlay.length === 2) && (cardsInPlay[0] !== cardsInPlay[1])) {
+  		alert("Sorry, try again.");
+	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	const cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	this.setAttribute('src',cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
 
-console.log("User flipped " + cards[cardId].rank)
-console.log(cards[cardId].cardImage)
-console.log(cards[cardId].suit)
-cardsInPlay.push(cards[cardId].rank)
-checkForMatch()
+	}
+
+	
+}
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+ 	cardElement.setAttribute('src', "images/back.png");
+ 	cardElement.setAttribute('data-id', i);
+ 	cardElement.addEventListener('click', flipCard);
+// As the last piece of the for loop, append the cardElement to the game board (which has an id of game-board).
+//Hint: use the appendChild() method to do this.
+	document.getElementById('game-board').appendChild(cardElement);
+
+	}
+
+}
+
+/* checkForMatch()
  if ((cardsInPlay.length === 2) && (cardsInPlay[0] === cardsInPlay[1])) {
 alert("You found a match!");
 }
 else if ((cardsInPlay.length === 2) && (cardsInPlay[0] !== cardsInPlay[1])) {
 alert("Sorry, try again.");
 }
-
-
 }
+*/
 
-flipCard(0)
-flipCard(2)
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
